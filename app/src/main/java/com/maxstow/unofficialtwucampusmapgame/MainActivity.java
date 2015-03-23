bbf7055870a4fbfe1020df0c48b157d986f139ea
Max <mstow@me.com>
2015-03-23 14:17:37 -0700

    Beginning development of GUI: Continued Mar 23, 2015 2:17 PM

--- app/src/main/java/com/maxstow/unofficialtwucampusmapgame/MainActivity.java
+++ app/src/main/java/com/maxstow/unofficialtwucampusmapgame/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.graphics.Color;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.res.Resources;
+import android.util.TypedValue;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,33 +20,96 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         //Layout
-        RelativeLayout mainLayout = new RelativeLayout(this);
-        mainLayout.setBackgroundColor(Color.DKGRAY);
+        RelativeLayout mainLayout;
+        mainLayout = new RelativeLayout(this);
+        mainLayout.setBackgroundColor(Color.GRAY);
 
         //Button
-        Button nameInputButton = new Button(this);
-        nameInputButton.setText("Confirm");
-        nameInputButton.setBackgroundColor(Color.LTGRAY);
+        Button userNameInputButton;
+        userNameInputButton = new Button(this);
+        userNameInputButton.setText("Confirm");
+        userNameInputButton.setBackgroundColor(Color.LTGRAY);
 
-        //Details on the formatting of the button
-        RelativeLayout.LayoutParams nameInputButtonDetails = new RelativeLayout.LayoutParams(
+        //Username input
+        EditText userNameInput;
+        userNameInput = new EditText(this);
+
+        //Username text instruction
+        TextView userNameTextInstruction;
+        userNameTextInstruction = new TextView(this);
+        userNameTextInstruction.setText("Please enter a name to be used for the app.");
+
+        //Username text printout
+        TextView userNamePrinted;
+        userNamePrinted = new TextView(this);
+
+        //Id allocation from id values given in the ids xml file
+        userNameInputButton.setId(R.id.userNameInputButton);
+        userNameInput.setId(R.id.userNameInput);
+        userNameTextInstruction.setId(R.id.userNameTextInstruction);
+        userNamePrinted.setId(R.id.userNamePrinted);
+
+        //Details on the formatting of the Button
+        RelativeLayout.LayoutParams userNameInputButtonDetails = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
 
-        //Adds rules to the details
-        nameInputButtonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        nameInputButtonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+        //Details on the formatting of the EditText
+        RelativeLayout.LayoutParams userNameInputDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //Details on the formatting of the TextView
+        RelativeLayout.LayoutParams userNameTextInstructionDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //Give rules to position the widgets in the Relative layout
+
+            //Button
+        userNameInputButtonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        userNameInputButtonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+
+            //EditText
+        userNameInputDetails.addRule(RelativeLayout.ABOVE, userNameInputButton.getId());
+        userNameInputDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        userNameInputDetails.setMargins(0,0,0,50);
+
+
+            //Convert DIP to pixels
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200,
+                r.getDisplayMetrics()
+        );
+
+        userNameInput.setWidth(px);
+
+            //TextView
+        userNameTextInstructionDetails.addRule(RelativeLayout.ABOVE, userNameInput.getId());
+        userNameTextInstructionDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        userNameTextInstructionDetails.setMargins(0,0,0,50);
+        userNameTextInstructionDetails.addRule(RelativeLayout.CENTER_VERTICAL);
 
 
         //Add widget to the layout(button is now a child of the layout)
-        mainLayout.addView(nameInputButton, nameInputButtonDetails);
+
+            //adds TextView to layout
+        mainLayout.addView(userNameTextInstruction, userNameTextInstructionDetails);
+
+            //adds EditText to layout
+        mainLayout.addView(userNameInput, userNameInputDetails);
+
+            //adds Button to layout
+        mainLayout.addView(userNameInputButton, userNameInputButtonDetails);
 
         //Set this activates
         setContentView(mainLayout);
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
