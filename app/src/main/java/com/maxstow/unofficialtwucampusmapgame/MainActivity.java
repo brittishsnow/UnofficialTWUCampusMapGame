b792ca3b0cac07dae0c055a68b2336a4c579feb3
Max <mstow@me.com>
2015-03-29 16:06:57 -0700

    Development of GUI: Continued Mar 29, 2015 4:06pm
    Setting Up Google Play Services

--- app/src/main/java/com/maxstow/unofficialtwucampusmapgame/MainActivity.java
+++ app/src/main/java/com/maxstow/unofficialtwucampusmapgame/MainActivity.java
@@ -17,12 +17,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, DebugIntentService.class);
-        startService(intent);
-
-        Intent i = new Intent(this, DebugService.class);
-        startService(i);
     }
 
     public void onClick(View view) {
