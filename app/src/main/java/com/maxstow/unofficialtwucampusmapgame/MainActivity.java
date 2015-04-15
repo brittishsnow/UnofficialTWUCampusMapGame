470882f383cf18f446820fdbc713c777d0fe6bde
Max <mstow@me.com>
2015-04-15 15:48:41 -0700

    Cleaning up imports

--- app/src/main/java/com/maxstow/unofficialtwucampusmapgame/MainActivity.java
+++ app/src/main/java/com/maxstow/unofficialtwucampusmapgame/MainActivity.java
@@ -1,15 +1,9 @@
 package com.maxstow.unofficialtwucampusmapgame;
 
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.TypedValue;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.content.Intent;
 
 public class MainActivity extends ActionBarActivity {
--- app/src/main/java/com/maxstow/unofficialtwucampusmapgame/Welcome.java
+++ app/src/main/java/com/maxstow/unofficialtwucampusmapgame/Welcome.java
@@ -1,12 +1,9 @@
 package com.maxstow.unofficialtwucampusmapgame;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Welcome extends ActionBarActivity {
